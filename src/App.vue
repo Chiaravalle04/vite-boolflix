34619e892c8f420a353c58f17d6ab2b4abeba593
Chiaravalle04 <nicolas.chiaravalle@gmail.com>
2023-02-08 16:05:49 +0100

    Milestone 2 Completata

--- src/App.vue
+++ src/App.vue
@@ -16,16 +16,20 @@ export default {
       axios
       .get('https://api.themoviedb.org/3/search/movie?api_key=9983fafc12ef7399a4bba69d8af1689f&query=' + this.store.searchMovies )
       .then((response) => {
-        // console.log(response.data.results[0].title)
         console.log(response.data)
-        this.store.results = response.data.results;
-        console.log(this.store.results)
-      } )
+        this.store.resultsMovie = response.data.results;
+        console.log('movies', this.store.resultsMovie)
+      })
+
+      axios
+      .get('https://api.themoviedb.org/3/search/tv?api_key=9983fafc12ef7399a4bba69d8af1689f&query=' + this.store.searchMovies )
+      .then((response) => {
+        console.log('series',response)
+        this.store.resultsSeries = response.data.results
+        console.log('arrayseries', this.store.resultsSeries)
+      })
     },
 
-    search() {
-
-    }
   },
 
   created() {
@@ -41,16 +45,46 @@ export default {
     <button type="button" @click="searchMovies()">Cerca</button>
   </div>
 
-  <div v-for="(item, index) in store.results">
-    <h2>Titolo : {{ store.results[index].title }}</h2>
-    <h2>Titolo originale: {{ store.results[index].original_title }}</h2>
-    <h2>Lingua originale: {{ store.results[index].original_language }}</h2>
-    <h2>Voto: {{ store.results[index].vote_average }}</h2>
+  <div class="feature-movies" v-for="(item, index) in store.resultsMovie">
+    <h2>Movie</h2>
+    <h2>Titolo : {{ store.resultsMovie[index].title }}</h2>
+    <h2>Titolo originale: {{ store.resultsMovie[index].original_title }}</h2>
+
+    <div class="container-languages">
+      <h2>Lingua originale:</h2>
+      <span :class="'fi' + ' ' + 'fi-' + 'gb'" v-if="store.resultsMovie[index].original_language == 'en'"></span>
+      <span :class="'fi' + ' ' + 'fi-' + store.resultsMovie[index].original_language" v-else></span>
+    </div>
+
+    <h2>Voto: {{ store.resultsMovie[index].vote_average }}</h2>
+    <hr>
+  </div>
+
+  <div class="feature-movies" v-for="(item, index) in store.resultsSeries">
+    <h2>Series</h2>
+    <h2>Titolo : {{ store.resultsSeries[index].name }}</h2>
+    <h2>Titolo originale: {{ store.resultsSeries[index].original_name }}</h2>
+
+    <div class="container-languages">
+      <h2>Lingua originale:</h2>
+      <span :class="'fi' + ' ' + 'fi-' + 'gb'" v-if="store.resultsSeries[index].original_language == 'en'"></span>
+      <span :class="'fi' + ' ' + 'fi-' + store.resultsSeries[index].original_language" v-else></span>
+    </div>
+
+    <h2>Voto: {{ store.resultsSeries[index].vote_average }}</h2>
     <hr>
   </div>
 
 </template>
 
 <style lang="scss">
-
+.feature-movies {
+  .container-languages {
+    display: flex;
+    span {
+      width: 70px;
+      margin-left: 20px;
+    }
+  }
+}
 </style>
